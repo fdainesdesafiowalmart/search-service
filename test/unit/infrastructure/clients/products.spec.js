492d7a54cb1c49c71f9dd50f407e01c741d62652
Francisco Daines <francisco.daines@2brains.cl>
2020-08-22 11:41:36 -0400

    Improves testing coverage

--- test/unit/infrastructure/clients/products.spec.js
+++ test/unit/infrastructure/clients/products.spec.js
@@ -6,19 +6,23 @@ const { getProducts, getProductById } = require('infrastructure/clients/products
 describe('Clients:Products', () => {
   it('should request to getProductById endpoint with provided id', async () => {
     process.env['PRODUCTS_SERVICE_URL'] = 'foobar'
+    process.env['PRODUCTS_SERVICE_APIKEY_HEADER'] = 'foo'
+    process.env['PRODUCTS_SERVICE_APIKEY_VALUE'] = 'bar'
     axios.get.mockImplementation(() => true)
 
     const result = await getProductById(1234)
 
-    expect(axios.get).toHaveBeenCalledWith('foobar/products/1234')
+    expect(axios.get).toHaveBeenCalledWith('foobar/products/1234', { headers: {'x-walmart-product-service-key': 'bar'}})
   })
 
   it('should request to getProducts endpoint with provided search pattern', async () => {
     process.env['PRODUCTS_SERVICE_URL'] = 'foobar'
+    process.env['PRODUCTS_SERVICE_APIKEY_HEADER'] = 'foo'
+    process.env['PRODUCTS_SERVICE_APIKEY_VALUE'] = 'bar'
     axios.get.mockImplementation(() => true)
 
     const result = await getProducts('hello+world')
 
-    expect(axios.get).toHaveBeenCalledWith('foobar/products?pattern=hello+world')
+    expect(axios.get).toHaveBeenCalledWith('foobar/products?pattern=hello+world', { headers: {'x-walmart-product-service-key': 'bar'}})
   })
 })
