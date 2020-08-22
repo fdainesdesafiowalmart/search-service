b0c05840bbe3e9dc5d68b59f902d84b7b8725aad
Francisco Daines <francisco.daines@2brains.cl>
2020-08-22 11:11:51 -0400

    Sends apikey header to products service

--- src/infrastructure/clients/products.js
+++ src/infrastructure/clients/products.js
@@ -1,17 +1,29 @@
 const axios = require('axios')
 
+const apiKeyHeader = process.env.PRODUCTS_SERVICE_APIKEY_HEADER || 'x-walmart-product-service-key'
+
+const resolveApiKeyHeaders = () => {
+  const headers = {}
+
+  headers[apiKeyHeader] = process.env.PRODUCTS_SERVICE_APIKEY_VALUE
+
+  return {
+    headers
+  }
+}
+
 const getProducts = async (pattern) => {
   const productsServiceUrl = process.env.PRODUCTS_SERVICE_URL
   const endpoint = `${productsServiceUrl}/products?pattern=${pattern}`
 
-  return axios.get(endpoint)
+  return axios.get(endpoint, resolveApiKeyHeaders())
 }
 
 const getProductById = async (id) => {
   const productsServiceUrl = process.env.PRODUCTS_SERVICE_URL
   const endpoint = `${productsServiceUrl}/products/${id}`
 
-  return axios.get(endpoint)
+  return axios.get(endpoint, resolveApiKeyHeaders())
 }
 
 module.exports = {
